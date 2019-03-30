0e07724b3aa36d3c8eeb31b9140072dec790c632
Lycheeeeeee <khanhniii07@gmail.com>
2019-03-30 15:03:55 +0700

    alla

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,13 +1,75 @@
 const mongoose = require("mongoose");
-const {hashPassword,enc,dec} = require("../models/userModel");
-const UserSchema = require("../models/userModel");
-// import {UserSchema} from "../models/userModel"
-const Users = mongoose.model('User',UserSchema);
+// const {hashPassword,enc,dec} = require("../models/userModel");
+// const {UserSchema} = require("../models/userModel");
+// // import {UserSchema} from "../models/userModel"
+// const Users = mongoose.model('User',UserSchema);
+var CryptoJS = require("crypto-js");
+// const mongoose = require("mongoose")
+const Schema = mongoose.Schema;
 
-const signUp = (req,res)=>{
+const UserSchema = new Schema({
+    userName:{
+        type:String,
+        required:'Enter username'
+    },
+    passWord:{
+        type:String,
+        required:'Enter password'
+    },
+    income:{
+        type:String,
+        required:'Enter income'
+    },
+    saving:{
+        type:Number,
+        required:'Enter saving amount,20% by default'
+    },
+    created_date:{
+        type:Date,
+        default:Date.now
+    }
+});
+// export function hashPassword(password){
+//     try {
+//         const salt = bcrypt.genSalt(10)
+//         return bcrypt.hash(password, salt)
+//       } catch(error) {
+//         throw new Error('Hashing failed', error)
+//       }
+// }
+// module.exports.hashPassword = async (password) => {
+//     try {
+//       const salt = await bcrypt.genSalt(10)
+//       return await bcrypt.hash(password, salt)
+//     } catch(error) {
+//       throw new Error('Hashing failed', error)
+//     }
+//   }
+function hashPassword(password){
+    var passwordHash = require('password-hash');
+    return passwordHash.generate(password);
+}
+
+function enc(data){
+    return CryptoJS.AES.encrypt(data, 'chiakhoa');
+}
+
+function dec(hash){
+    var bytes  = CryptoJS.AES.decrypt(hash, 'chiakhoa');
+    return  bytes.toString(CryptoJS.enc.Utf8);
+}
+// module.exports = {UserSchema};
+// module.exports = hashPassword;
+// module.exports = enc;
+// module.exports = dec;
+
+
+const User = mongoose.model('User',UserSchema);
+
+exports.signUp = (req,res)=>{
     req.body.passWord = hashPassword(req.body.passWord);
     req.body.income = enc(req.body.income);
-    let newUser = new Users(req.body);
+    let newUser = new User(req.body);
     User.findOne({'userName':`${req.body.userName}`},(err,usr)=>{
         if(usr){
             res.json({'message':'Username exsit, please enter other one'})
@@ -23,9 +85,9 @@ const signUp = (req,res)=>{
         )
     })
 };
-const logIn =(req,res)=>{
+exports.logIn =(req,res)=>{
     var passwordHash = require('password-hash');
-    let checkUser = new Users(req.body);
+    let checkUser = new User(req.body);
     User.findOne({'userName':`${req.body.userName}`},(err,usr)=>{
         if(passwordHash.verify(req.body.passWord,usr.passWord)){
             usr.income = dec(usr.income);
@@ -36,7 +98,6 @@ const logIn =(req,res)=>{
     }
     })
 };
-module.exports = signUp;
-module.exports = logIn;
+
 
 
--- src/models/userModel.js
+++ src/models/userModel.js
@@ -1,62 +1,62 @@
-var CryptoJS = require("crypto-js");
-const mongoose = require("mongoose")
-const Schema = mongoose.Schema;
+// var CryptoJS = require("crypto-js");
+// const mongoose = require("mongoose")
+// const Schema = mongoose.Schema;
 
-const UserSchema = new Schema({
-    userName:{
-        type:String,
-        required:'Enter username'
-    },
-    passWord:{
-        type:String,
-        required:'Enter password'
-    },
-    income:{
-        type:String,
-        required:'Enter income'
-    },
-    saving:{
-        type:Number,
-        required:'Enter saving amount,20% by default'
-    },
-    created_date:{
-        type:Date,
-        default:Date.now
-    }
-});
-// export function hashPassword(password){
-//     try {
-//         const salt = bcrypt.genSalt(10)
-//         return bcrypt.hash(password, salt)
-//       } catch(error) {
-//         throw new Error('Hashing failed', error)
-//       }
-// }
-// module.exports.hashPassword = async (password) => {
-//     try {
-//       const salt = await bcrypt.genSalt(10)
-//       return await bcrypt.hash(password, salt)
-//     } catch(error) {
-//       throw new Error('Hashing failed', error)
+// const UserSchema = new Schema({
+//     userName:{
+//         type:String,
+//         required:'Enter username'
+//     },
+//     passWord:{
+//         type:String,
+//         required:'Enter password'
+//     },
+//     income:{
+//         type:String,
+//         required:'Enter income'
+//     },
+//     saving:{
+//         type:Number,
+//         required:'Enter saving amount,20% by default'
+//     },
+//     created_date:{
+//         type:Date,
+//         default:Date.now
 //     }
-//   }
-function hashPassword(password){
-    var passwordHash = require('password-hash');
-    return passwordHash.generate(password);
-}
+// });
+// // export function hashPassword(password){
+// //     try {
+// //         const salt = bcrypt.genSalt(10)
+// //         return bcrypt.hash(password, salt)
+// //       } catch(error) {
+// //         throw new Error('Hashing failed', error)
+// //       }
+// // }
+// // module.exports.hashPassword = async (password) => {
+// //     try {
+// //       const salt = await bcrypt.genSalt(10)
+// //       return await bcrypt.hash(password, salt)
+// //     } catch(error) {
+// //       throw new Error('Hashing failed', error)
+// //     }
+// //   }
+// function hashPassword(password){
+//     var passwordHash = require('password-hash');
+//     return passwordHash.generate(password);
+// }
 
-function enc(data){
-    return CryptoJS.AES.encrypt(data, 'chiakhoa');
-}
+// function enc(data){
+//     return CryptoJS.AES.encrypt(data, 'chiakhoa');
+// }
 
-function dec(hash){
-    var bytes  = CryptoJS.AES.decrypt(hash, 'chiakhoa');
-    return  bytes.toString(CryptoJS.enc.Utf8);
-}
-module.exports = UserSchema;
-module.exports = hashPassword;
-module.exports = enc;
-module.exports = dec;
+// function dec(hash){
+//     var bytes  = CryptoJS.AES.decrypt(hash, 'chiakhoa');
+//     return  bytes.toString(CryptoJS.enc.Utf8);
+// }
+// module.exports = {UserSchema};
+// module.exports = hashPassword;
+// module.exports = enc;
+// module.exports = dec;
 
 
 
